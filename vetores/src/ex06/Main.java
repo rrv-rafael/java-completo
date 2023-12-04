f9ddc72fe693889a47959642087b8ecc6c9bef53
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2023-12-03 22:59:29 -0300

    Exercício 6 finalizado.

---
+++ vetores/src/ex06/Main.java
@@ -0,0 +1,39 @@
+package ex06;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Quantos terão cada vetor? ");
+        int tamanhoVetor = scan.nextInt();
+
+        int[] vetorA = new int[tamanhoVetor];
+        int[] vetorB = new int[tamanhoVetor];
+        int[] vetorC = new int[tamanhoVetor];
+
+        System.out.println("Informe o valor para o vetor A:");
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] = scan.nextInt();
+        }
+
+        System.out.println("Informe o valor para o vetor B: ");
+
+        for (int i = 0; i < vetorB.length; i++) {
+            vetorB[i] = scan.nextInt();
+        }
+
+        System.out.println("VETOR RESULTANTE:");
+
+        for (int i = 0; i < vetorC.length; i++) {
+            vetorC[i] = vetorA[i] + vetorB[i];
+
+            System.out.println(vetorC[i]);
+        }
+
+        scan.close();
+    }
+}
