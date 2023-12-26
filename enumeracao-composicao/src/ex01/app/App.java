c7ea32baf7fa30335c6f5e9f80b29c4c17cfab0a
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2023-12-26 16:10:07 -0300

    Exercício finalizado.

--- enumeracao-composicao/src/ex01/app/App.java
+++ enumeracao-composicao/src/ex01/app/App.java
@@ -27,19 +27,19 @@ public class App {
         System.out.print("Nome: ");
         String nome = scan.nextLine();
         System.out.print("Nível: ");
-        NivelFuncionario nivelFuncionario = NivelFuncionario.valueOf(scan.nextLine());
+        String nivelFuncionario = scan.nextLine();
         System.out.print("Salário base: ");
         double salarioBase = scan.nextDouble();
+
+        Funcionario funcionario = new Funcionario(nome, NivelFuncionario.valueOf(nivelFuncionario), salarioBase, departamento);
+
         System.out.print("\nQuantos contratos para este funcionário: ");
         int qtdContrato = scan.nextInt();
-
-        Funcionario funcionario = new Funcionario(nome, nivelFuncionario, salarioBase, departamento);
 
         for (int i = 0; i < qtdContrato; i++) {
             System.out.printf("\nInforme os dados do contrato #%d:\n", i + 1);
             System.out.print("Data (DD/MM/YYYY): ");
-            scan.nextLine();
-            String dataInformada = scan.nextLine();
+            String dataInformada = scan.next();
             System.out.print("Valor por hora: ");
             double valorHora = scan.nextDouble();
             System.out.print("Duração (em horas): ");
@@ -53,16 +53,13 @@ public class App {
         }
 
         System.out.print("\nInforme o mês e ano para calcular o rendimento (MM/YYYY): ");
-        scan.nextLine();
-        String dataInformada = scan.nextLine();
+        String dataInformada = scan.next();
         LocalDate localDate = LocalDate.parse("01/" + dataInformada, formatter);
         int mes = localDate.getMonthValue();
         int ano = localDate.getYear();
 
-        Double rendimentos = funcionario.rendimento(ano, mes);
-
         System.out.println(funcionario);
-        System.out.printf("Rendimentos referente à %s: %.2f", dataInformada, rendimentos);
+        System.out.printf("Rendimentos referente à %s: %.2f", dataInformada, funcionario.rendimento(ano, mes));
 
         scan.close();
     }
