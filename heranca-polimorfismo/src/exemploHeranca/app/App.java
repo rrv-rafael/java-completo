f778189634cd25bf01be0f9f662ad452add3074f
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2023-12-28 17:53:44 -0300

    Aula sobre UPCASTING e DOWNCASTING.

--- heranca-polimorfismo/src/exemploHeranca/app/App.java
+++ heranca-polimorfismo/src/exemploHeranca/app/App.java
@@ -2,14 +2,35 @@ package exemploHeranca.app;
 
 import exemploHeranca.entidades.Conta;
 import exemploHeranca.entidades.ContaEmpresarial;
+import exemploHeranca.entidades.ContaPoupanca;
 
 public class App {
     public static void main(String[] args) {
-        ContaEmpresarial contaEmpresarial = new ContaEmpresarial(1, "Rafael", 10.0, 100.0);
+        Conta conta = new Conta(1001, "Alex", 0.0);
 
-        contaEmpresarial.realizarDeposito(2);
-        double saldo = contaEmpresarial.getSaldo();
+        ContaEmpresarial contaEmpresarial = new ContaEmpresarial(1002, "Maria", 0.0, 500.0);
 
-        System.out.println(saldo);
+        // UPCASTING:
+        Conta conta1 = contaEmpresarial;
+        Conta conta2 = new ContaEmpresarial(1003, "Bob", 0.0, 200.0);
+        Conta conta3 = new ContaPoupanca(1004, "Ana", 0.0, 0.01);
+
+        // DOWNCASTING
+        ContaEmpresarial conta4 = (ContaEmpresarial) conta2;
+        conta4.emprestimo(100.0);
+
+        //ContaEmpresarial conta5 = (ContaEmpresarial) conta3;
+
+        if (conta3 instanceof ContaEmpresarial) {
+            ContaEmpresarial conta5 = (ContaEmpresarial) conta3;
+            conta5.emprestimo(200.0);
+            System.out.println("Emprestado.");
+        }
+
+        if (conta3 instanceof ContaPoupanca) {
+            ContaPoupanca conta5 = (ContaPoupanca) conta3;
+            conta5.atualizarSaldo();
+            System.out.println("Atualizado.");
+        }
     }
 }
---
+++ heranca-polimorfismo/src/exemploHeranca/entidades/ContaPoupanca.java
@@ -0,0 +1,25 @@
+package exemploHeranca.entidades;
+
+public class ContaPoupanca extends Conta {
+    private Double taxaJuros;
+
+    public ContaPoupanca() {
+    }
+
+    public ContaPoupanca(Integer numero, String titular, Double saldo, Double taxaJuros) {
+        super(numero, titular, saldo);
+        this.taxaJuros = taxaJuros;
+    }
+
+    public Double getTaxaJuros() {
+        return taxaJuros;
+    }
+
+    public void setTaxaJuros(Double taxaJuros) {
+        this.taxaJuros = taxaJuros;
+    }
+
+    public void atualizarSaldo() {
+        saldo += saldo * taxaJuros;
+    }
+}
