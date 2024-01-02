537be127cc7b9429481f755f45a731ae59b4b100
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-01-02 19:34:33 -0300

    Código refatorado.

--- tratamento-excecoes/src/ex01/app/App.java
+++ tratamento-excecoes/src/ex01/app/App.java
@@ -3,44 +3,49 @@ package ex01.app;
 import ex01.model.entidades.Reserva;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
+    public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner scan = new Scanner(System.in);
-
-        String dataEntradaInformada, dataSaidaInformada;
-        LocalDate dataEntrada, dataSaida;
 
         System.out.print("Número do quarto: ");
         int numeroQuarto = scan.nextInt();
-        System.out.print("Data de entrada (dd/MM/yyyy): ");
-        dataEntradaInformada = scan.next();
-        System.out.print("Data de saída (dd/MM/yyyy): ");
-        dataSaidaInformada = scan.next();
 
-        dataEntrada = LocalDate.parse(dataEntradaInformada, Reserva.FORMATTER);
-        dataSaida = LocalDate.parse(dataSaidaInformada, Reserva.FORMATTER);
+        List<LocalDate> datas = lerData();
 
-        Reserva reserva = new Reserva(numeroQuarto, dataEntrada, dataSaida);
+        Reserva reserva = new Reserva(numeroQuarto, datas.getFirst(), datas.getLast());
 
         System.out.println(reserva);
 
         System.out.println("\nEntre com a data para atualizar sua reserva:");
-        System.out.print("Data de entrada (dd/MM/yyyy): ");
-        dataEntradaInformada = scan.next();
-        System.out.print("Data de saída (dd/MM/yyyy): ");
-        dataSaidaInformada = scan.next();
 
-        dataEntrada = LocalDate.parse(dataEntradaInformada, Reserva.FORMATTER);
-        dataSaida = LocalDate.parse(dataSaidaInformada, Reserva.FORMATTER);
+        datas = lerData();
 
-        reserva.atualizarData(dataEntrada, dataSaida);
+        reserva.atualizarData(datas.getFirst(), datas.getLast());
         System.out.println(reserva);
 
         scan.close();
+    }
+
+    public static List<LocalDate> lerData() {
+        System.out.print("Data de entrada (dd/MM/yyyy): ");
+        String dataEntradaInformada = scan.next();
+        System.out.print("Data de saída (dd/MM/yyyy): ");
+        String dataSaidaInformada = scan.next();
+
+        LocalDate dataEntrada = LocalDate.parse(dataEntradaInformada, Reserva.FORMATTER);
+        LocalDate dataSaida = LocalDate.parse(dataSaidaInformada, Reserva.FORMATTER);
+
+        List<LocalDate> datas = new ArrayList<>();
+
+        datas.add(dataEntrada);
+        datas.add(dataSaida);
+
+        return datas;
     }
 }
