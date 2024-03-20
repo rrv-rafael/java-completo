5ec8a49ae971e4605187713f3e668e0ec1f77aa0
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-03-19 21:17:14 -0300

    Recuperando dados da base.

--- banco-dados-jdbc/src/application/App.java
+++ banco-dados-jdbc/src/application/App.java
@@ -3,10 +3,33 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
 
 public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
     public static void main(String[] args) {
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DB.getConnection();
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery("SELECT * FROM department");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Id") + " - " + resultSet.getString("Name"));
+            }
+        } catch (SQLException e) {
+            logger.severe("Ocorreu o seguinte erro: " + e.getMessage());
+        } finally {
+            DB.closeResultSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
     }
 }
