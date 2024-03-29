a1b322878c91cc6b5559ad399f3cea250f2ea098
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-03-29 10:15:07 -0300

    Criação da classe vendedor.

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,11 +1,20 @@
 package application;
 
 import model.entidades.Departamento;
+import model.entidades.Vendedor;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
     public static void main(String[] args) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         Departamento departamento = new Departamento(1, "Livros");
 
-        System.out.println(departamento);
+
+
+        Vendedor vendedor = new Vendedor(1, "Rafael Rodrigues Vitor", "rafael.vitor@gmail.com", LocalDate.parse("12/11/1992", dateTimeFormatter), 3.000, departamento);
+
+        System.out.println(vendedor);
     }
 }
---
+++ demo-dao-jdbc/src/model/entidades/Vendedor.java
@@ -0,0 +1,96 @@
+package model.entidades;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Vendedor {
+    private Integer id;
+    private String nome;
+    private String email;
+    private LocalDate dataNascimento;
+    private Double baseSalarial;
+
+    private Departamento departamento;
+
+    public Vendedor(Integer id, String nome, String email, LocalDate dataNascimento, Double baseSalarial, Departamento departamento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.baseSalarial = baseSalarial;
+        this.departamento = departamento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Double getBaseSalarial() {
+        return baseSalarial;
+    }
+
+    public void setBaseSalarial(Double baseSalarial) {
+        this.baseSalarial = baseSalarial;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vendedor vendedor = (Vendedor) o;
+        return Objects.equals(id, vendedor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", baseSalarial=" + baseSalarial +
+                ", departamento=" + departamento +
+                '}';
+    }
+}
