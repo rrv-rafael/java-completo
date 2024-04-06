c7fb4e5f2b9f9c571c9b269db0469378b13edbef
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-04-06 15:55:06 -0300

    Introdução ao JPA finalizada.

--- jpa-hibernate-maven/src/main/java/aplicacao/Program.java
+++ jpa-hibernate-maven/src/main/java/aplicacao/Program.java
@@ -2,14 +2,47 @@ package aplicacao;
 
 import dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Program {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+        /*
+        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+        */
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        //Buscando dados no banco:
+        Pessoa p = entityManager.find(Pessoa.class, 2);
+
+        entityManager.getTransaction().begin();
+
+        //Deletando registro no banco:
+        entityManager.remove(p);
+
+        entityManager.getTransaction().commit();
+
+        System.out.println(p);
+
+        /*
+        Inserindo dados no banco:
+        entityManager.getTransaction().begin();
+
+
+        entityManager.persist(p1);
+        entityManager.persist(p2);
+        entityManager.persist(p3);
+
+        entityManager.getTransaction().commit();
+        */
+        System.out.println("Pronto.");
+
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
--- jpa-hibernate-maven/src/main/java/dominio/Pessoa.java
+++ jpa-hibernate-maven/src/main/java/dominio/Pessoa.java
@@ -1,11 +1,19 @@
 package dominio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer codPessoa;
     private String nome;
     private String email;
