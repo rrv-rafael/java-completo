1b2103c7f0f07b2122e939d224deacbf3fb1fd5f
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-04-07 11:21:14 -0300

    Criação do projeto.

---
+++ web-service-spring-boot/src/main/java/com/rrv/webservicespringboot/WebServiceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rrv.webservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringBootApplication.class, args);
+	}
+
+}
---
+++ web-service-spring-boot/src/test/java/com/rrv/webservicespringboot/WebServiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrv.webservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
