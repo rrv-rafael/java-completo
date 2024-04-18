6957af654ba0c75be2bd6deea3381bd18ac6a822
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-04-17 21:10:22 -0300

    Criação do projeto.

---
+++ webservice-mongodb/src/test/java/com/rrv/webservicemongodb/WebserviceMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrv.webservicemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
