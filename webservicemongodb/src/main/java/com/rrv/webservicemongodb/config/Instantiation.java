3fa5f4c96258cfdf3d30b6df86d7470c636e2032
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-04-23 19:45:50 -0300

    Criando a entidade Post com Usuario aninhado.

--- webservicemongodb/src/main/java/com/rrv/webservicemongodb/config/Instantiation.java
+++ webservicemongodb/src/main/java/com/rrv/webservicemongodb/config/Instantiation.java
@@ -1,26 +1,43 @@
 package com.rrv.webservicemongodb.config;
 
+import com.rrv.webservicemongodb.domain.Post;
 import com.rrv.webservicemongodb.domain.Usuario;
+import com.rrv.webservicemongodb.repository.PostRepository;
 import com.rrv.webservicemongodb.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
         usuarioRepository.deleteAll();
+        postRepository.deleteAll();
 
         Usuario maria = new Usuario(null, "Maria Brown", "maria@gmail.com");
         Usuario alex = new Usuario(null, "Alex Green", "alex@gmail.com");
         Usuario bob = new Usuario(null, "Bob Grey", "bob@gmail.com");
 
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        Post post1 = new Post(null, LocalDate.parse("21/03/2018", dateTimeFormatter), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, LocalDate.parse("18/03/2018", dateTimeFormatter), "Bom dia", "Acordei feliz hoje!", maria);
+
         usuarioRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ webservicemongodb/src/main/java/com/rrv/webservicemongodb/domain/Post.java
@@ -0,0 +1,88 @@
+package com.rrv.webservicemongodb.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Document
+public class Post implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String codPost;
+    private LocalDate data;
+    private String titulo;
+    private String corpo;
+
+    private Usuario autor;
+
+    public Post() {
+    }
+
+    public Post(String codPost, LocalDate data, String titulo, String corpo, Usuario autor) {
+        this.codPost = codPost;
+        this.data = data;
+        this.titulo = titulo;
+        this.corpo = corpo;
+        this.autor = autor;
+    }
+
+    public String getCodPost() {
+        return codPost;
+    }
+
+    public void setCodPost(String codPost) {
+        this.codPost = codPost;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCorpo() {
+        return corpo;
+    }
+
+    public void setCorpo(String corpo) {
+        this.corpo = corpo;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(codPost, post.codPost);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codPost);
+    }
+}
